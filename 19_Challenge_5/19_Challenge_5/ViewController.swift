1ef630d6d08f720efb6194451c877de97f521b7f
Milo <Milo.CTT@gmail.com>
2019-07-22 18:31:34 +0800

    Update

--- 19_Challenge_5/19_Challenge_5/ViewController.swift
+++ 19_Challenge_5/19_Challenge_5/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController,UITextFieldDelegate {
     let fullScrenn = UIScreen.main.bounds.size
     let login = UIButton()
     let label = UILabel()
+    var okAction: UIAlertAction! = nil
+    var acc = UITextField()
+    var password = UITextField()
+    var alertController:UIAlertController!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +31,12 @@ class ViewController: UIViewController,UITextFieldDelegate {
         
         label.frame = CGRect(x: 0, y: 0, width: fullScrenn.width * 0.9, height: 100)
         label.center = CGPoint(x: fullScrenn.width * 0.5, y: fullScrenn.height * 0.3)
-//        label.backgroundColor = UIColor.lightGray
         label.font = label.font.withSize(20)
         label.textAlignment = .center
         self.view.addSubview(label)
         
         login.addTarget(self, action: #selector(ViewController.signIn), for: .touchUpInside)
-
-    }
+        }
     
     //點擊retrun收起鍵盤
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -46,9 +49,17 @@ class ViewController: UIViewController,UITextFieldDelegate {
         self.view.endEditing(true)
     }
     
+    //當帳號密碼都輸入時才可以按登入鈕
+    @objc func textFieldIsEmpty(){
+        let textField0: UITextField  = alertController.textFields![0]
+        let textField1: UITextField  = alertController.textFields![1]
+        alertController.actions[1].isEnabled = !textField0.text!.isEmpty && !textField1.text!.isEmpty
+    }
+    
+    
     @objc func signIn(){
         let mypassword = "Milo"
-        let alertController = UIAlertController(
+        alertController = UIAlertController(
             title: "登入",
             message: "請輸入帳號與密碼",
             preferredStyle: .alert)
@@ -57,12 +68,14 @@ class ViewController: UIViewController,UITextFieldDelegate {
         alertController.addTextField {
             (textField: UITextField!) -> Void in
             textField.placeholder = "帳號"
+            textField.addTarget(self, action: #selector(ViewController.textFieldIsEmpty), for: .editingChanged)
         }
         alertController.addTextField {
             (textField: UITextField!) -> Void in
             textField.placeholder = "密碼"
             // 如果要將textField顯示為密碼樣式，那這個屬性要設定為 true
             textField.isSecureTextEntry = true
+            textField.addTarget(self, action: #selector(ViewController.textFieldIsEmpty), for: .editingChanged)
         }
         
         // 建立[取消]按鈕
@@ -73,15 +86,15 @@ class ViewController: UIViewController,UITextFieldDelegate {
         alertController.addAction(cancelAction)
         
         // 建立[登入]按鈕
-        let okAction = UIAlertAction(
+        okAction = UIAlertAction(
             title: "登入",
-            style: .default) {
+            style: .default)
+            {
                 (action: UIAlertAction!) -> Void in
-                let acc = (alertController.textFields?.first)! as UITextField
-                let password = (alertController.textFields?.last)! as UITextField
-                let temp = String(password.text!)
-                
-                if acc.text!.isEmpty || password.text!.isEmpty{
+                self.acc = (self.alertController.textFields?.first)! as UITextField
+                self.password = (self.alertController.textFields?.last)! as UITextField
+                let temp = String(self.password.text!)
+                if self.acc.text!.isEmpty || self.password.text!.isEmpty{
                    self.label.text = "請輸入完整的帳號及密碼缺一不可"
                 }
                 else if temp == mypassword {
@@ -92,9 +105,10 @@ class ViewController: UIViewController,UITextFieldDelegate {
                     self.label.textColor = .red
                     self.label.text = "密碼錯誤"
                 }
-                print("輸入的帳號為：\(acc.text!)")
-                print("輸入的密碼為：\(password.text!)")
+                print("輸入的帳號為：\(self.acc.text!)")
+                print("輸入的密碼為：\(self.password.text!)")
         }
+        okAction.isEnabled = false
         alertController.addAction(okAction)
         self.present(alertController, animated: true, completion: nil)
     }
